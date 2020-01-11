dd66a2c56d78f46f9ef3a8884b66c16f2c0a9b3a
Plamena Topacharova <plamt@hotmail.com>
2020-01-12 01:47:57 +0200

    merging

--- javaee11/src/main/java/pisibg/ittalents/model/pojo/User.java
+++ javaee11/src/main/java/pisibg/ittalents/model/pojo/User.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Component;
 import pisibg.ittalents.model.dto.RegisterUserDTO;
 
 import javax.persistence.*;
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
